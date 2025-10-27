7108786ff30ae2faa85a49b6eda7b70162713d2b
plan <frostyplanet@gmail.com>
2025-10-27 21:46:38 +0800

    Remove logger setup and rstest
    To isolate issue

--- tests/pressure.rs
+++ tests/pressure.rs
@@ -1,27 +1,19 @@
 use atomic_waitgroup::WaitGroup;
 use rand::{rngs::OsRng, RngCore};
 use std::time::Duration;
-use captains_log::{logfn, *};
-use rstest::*;
 
 mod common;
 use common::*;
 
-#[fixture]
-fn setup_log() {
-    _setup_log();
-}
-
-#[logfn]
-#[rstest]
-fn pressure_wait_group_loop(setup_log: ()) {
+#[test]
+fn pressure_wait_group_loop() {
     let wg = WaitGroup::new();
     runtime_block_on!(1, async move {
         let mut loop_cnt = 0;
         for _ in 0..1000 {
             let threads = OsRng.next_u32() % 10 + 1;
             loop_cnt += 1;
-            info!("loop_cnt={} threads={}", loop_cnt, threads);
+            println!("loop_cnt={} threads={}", loop_cnt, threads);
 
             for _i in 0..threads {
                 wg.add(1);
